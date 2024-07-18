ce5d25f57f0d5e52508bffd871e5962b36835158
Stephany Milhomem <stephanymilhomem25@gmail.com>
2024-07-18 10:28:43 -0300

    Fixing security configurations

--- src/main/java/cs2024/inf/CineView/config/SecurityConfig.java
+++ src/main/java/cs2024/inf/CineView/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -35,7 +36,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**", "/users", "/profile/**").permitAll() // Permita acesso a /auth, /users e /profile
+                        .requestMatchers("/auth/**").permitAll() // Permita acesso a /auth
                         .anyRequest().authenticated()); // Protege outras rotas
         return http.build();
     }
