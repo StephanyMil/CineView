5cd9c698ffb940ac9aff2fe1f5d863dff084088c
Stephany Milhomem <stephanymilhomem25@gmail.com>
2024-07-19 12:37:00 -0300

    Fix permissions

--- src/main/java/cs2024/inf/CineView/config/SecurityConfig.java
+++ src/main/java/cs2024/inf/CineView/config/SecurityConfig.java
@@ -1,15 +1,11 @@
 package cs2024.inf.CineView.config;
 
-import cs2024.inf.CineView.services.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,9 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
-    private UserDetailsServiceImpl userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -36,8 +29,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**").permitAll() // Permita acesso a /auth
-                        .anyRequest().authenticated()); // Protege outras rotas
+                        .anyRequest().permitAll()); // Permite todas as requisições
         return http.build();
     }
 }
