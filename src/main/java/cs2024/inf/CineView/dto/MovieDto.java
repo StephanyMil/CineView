504f2a248144da9e4f6c51e0b1246a3c8867fe74
Ayumi Hamano <98706488+AyuHamano@users.noreply.github.com>
2024-07-22 16:45:54 -0300

    feat(filmes): limpando o model de filmes

--- src/main/java/cs2024/inf/CineView/dto/MovieDto.java
+++ src/main/java/cs2024/inf/CineView/dto/MovieDto.java
@@ -1,11 +1,12 @@
 package cs2024.inf.CineView.dto;
 
-import cs2024.inf.CineView.models.Genre;
+import cs2024.inf.CineView.models.GenreModel;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 import java.util.List;
 
 public record MovieDto(@NotNull String title, @NotNull String overview, @NotNull Date releaseDate,
-                       @NotNull float voteAverage, @NotNull int runtime, @NotNull List<Genre> genres, String tagline) {
+                       @NotNull float voteAverage, @NotNull int runtime, @NotNull List<GenreModel> genreModels,
+                       String tagline) {
 }
